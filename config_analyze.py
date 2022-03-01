91cd44b75025b49c3e946ff1de1644f5172d59a1
inbalpreuss <inbalpreuss@gmail.com>
2022-03-01 09:17:03 +0200

    Added results count + 5 most common values from payload for each bc. (Getting the Sigma)

--- config_analyze.py
+++ config_analyze.py
@@ -3,8 +3,10 @@ config = {
     'design_len': 220,
     'payload_len': 20,
     'universal_len': 20,
+    'barcode_len': 20,
     'input_file': "D:\\Downloads\\merged_robot_two_BCs\\merged_robot_two_BCs.fastq",
     'const_design_file': "D:\\Downloads\\merged_robot_two_BCs\\design.csv",
+    'barcodes_design_file': "D:\\Downloads\\merged_robot_two_BCs\\barcodes_design.csv",
     'payload_design_file': "D:\\Downloads\\merged_robot_two_BCs\\payload_design.csv",
     'data_csv': "D:\\Downloads\\merged_robot_two_BCs\\data\\csv\\",
     'data_hist': "D:\\Downloads\\merged_robot_two_BCs\\data\\histograms\\"
