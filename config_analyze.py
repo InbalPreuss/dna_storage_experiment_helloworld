99fb6e6fbc65109119cfab3657a62d83c53691b2
inbal <inbalpreuss@gmail.com>
2022-08-04 17:38:06 +0300

    1. added design files
    2. changed paths in the config_analyze to have the new paths to the desgin + output folders

--- config_analyze.py
+++ config_analyze.py
@@ -7,12 +7,22 @@ config = {
     'barcode_len': 20,
     'subset_size': 5,
     'amount_of_payloads': 16,
-    'input_file': "D:\\Downloads\\merged_robot_two_BCs\\merged_robot_two_BCs.fastq",
-    'const_design_file': "D:\\Downloads\\merged_robot_two_BCs\\design.csv",
-    'barcodes_design_file': "D:\\Downloads\\merged_robot_two_BCs\\barcodes_design.csv",
-    'payload_design_file': "D:\\Downloads\\merged_robot_two_BCs\\payload_design.csv",
-    'data_csv': "D:\\Downloads\\merged_robot_two_BCs\\data\\csv\\",
-    'data_hist': "D:\\Downloads\\merged_robot_two_BCs\\data\\histograms\\"
+
+    # # 2 BC analysis
+    # 'input_file': "D:\\Downloads\\merged_robot_two_BCs\\merged_robot_two_BCs.fastq",
+    # 'const_design_file': "D:\\Downloads\\merged_robot_two_BCs\\design.csv",
+    # 'barcodes_design_file': "D:\\Downloads\\merged_robot_two_BCs\\barcodes_design.csv",
+    # 'payload_design_file': "D:\\Downloads\\merged_robot_two_BCs\\payload_design.csv",
+    # 'data_csv': "D:\\Downloads\\merged_robot_two_BCs\\data\\csv\\",
+    # 'data_hist': "D:\\Downloads\\merged_robot_two_BCs\\data\\histograms\\",
+
+    # 167 BC analysis
+    'input_file': "data/output_full_167bc_.assembled.fastq",
+    'const_design_file': "config/design.csv",
+    'barcodes_design_file': "config/barcodes_design.csv",
+    'payload_design_file': "config/payload_design.csv",
+    'data_csv': "output/csv/",
+    'data_hist': "output/hist/"
 }
 
 
